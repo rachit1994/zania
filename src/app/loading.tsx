7ebcb96e437c7952b8a705c29dc1ab6e3ea15e64
rachit1994 <rachit.srivastava1994@gmail.com>
2024-04-17 15:32:33 +0530

    images with placeholder

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,12 @@
+export const ImageLoader = () => <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
+
 const Loading = () => {
   return (
     <div className="flex h-screen items-center justify-center w-full">
       <div className="grid grid-cols-3 gap-4 w-full">
-        {/* First row of 3 cards */}
-        <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
-        <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
-        <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
-        {/* Second row of 2 cards, centered */}
-        <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
-        <div className="col-span-1 animate-pulse rounded-lg bg-gray-700 h-64"></div>
+        {[...Array(5)].map((_, index) => (
+          <ImageLoader key={`image-loader-${index}`} />
+        ))}
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { base64Str } from "@/components/Loaders/imageFallbackLoader";
+import Image from "next/image";
 import useSWR from "swr";
 import Loading from "./loading";
 
@@ -16,7 +18,26 @@ const Home = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex h-screen items-center justify-center w-full">
+        <div className="grid grid-cols-3 gap-4 w-full">
+          {data?.map((item: any) => {
+            return (
+              <div key={item.position} className="col-span-1 rounded-lg h-64">
+                <Image
+                  src={`/images/${item.position}.webp`}
+                  alt={item.title}
+                  width={100}
+                  height={100}
+                  className="w-full h-full rounded-lg"
+                  placeholder={base64Str}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
   );
 };
 
