35df1fe17690cf8121ad17999706792c4b531675
rachit1994 <rachit.srivastava1994@gmail.com>
2024-04-19 00:50:43 +0530

    image fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,11 @@ import { base64Str } from "@/components/Loaders/imageFallbackLoader";
 import Overlay from "@/components/Overlay";
 import { MyData } from "@/lib/features/home/fallbackJson";
 import Image from "next/image";
-import { usePathname } from 'next/navigation';
 import { memo } from "react";
 
 const Card = ({ item }: { item: MyData }) => {
-  const pathname = usePathname();
-  console.log('pathname:', pathname);
+  const pathname = window?.location?.pathname;
+  console.log('pathname:', window?.location?.pathname);
   return (
     <Overlay>
       <h3 className="text-left">{item.title}</h3>
