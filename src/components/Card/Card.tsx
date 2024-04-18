708854fe193a8a8043e548c9268666ec6a1c7e6b
rachit1994 <rachit.srivastava1994@gmail.com>
2024-04-19 01:06:47 +0530

    maybe image fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,15 +3,26 @@ import Overlay from "@/components/Overlay";
 import { MyData } from "@/lib/features/home/fallbackJson";
 import Image from "next/image";
 import { memo } from "react";
+import Image0 from '../../../public/images/0.webp';
+import Image1 from '../../../public/images/1.webp';
+import Image2 from '../../../public/images/2.webp';
+import Image3 from '../../../public/images/3.webp';
+import Image4 from '../../../public/images/4.webp';
+
+const imagesMap = {
+ 0: Image0,
+ 1: Image1,
+ 2: Image2,
+ 3: Image3,
+ 4: Image4,
+};
 
 const Card = ({ item }: { item: MyData }) => {
-  const pathname = window?.location?.pathname;
-  console.log('pathname:', window?.location?.pathname);
   return (
     <Overlay>
       <h3 className="text-left">{item.title}</h3>
         <Image
-          src={`${pathname === '/' ? '/' : `${pathname}/`}images/${item.position}.webp`}
+          src={imagesMap[item.position as keyof typeof imagesMap]}
           alt={item.title}
           width={100}
           height={100}
